986e0daa1b3e21692e84dedf572810918087d785
enzorusso <enzorussor1@gmail.com>
2022-06-04 18:18:02 -0300

    Init repo

---
+++ server/IServerClock.java
@@ -0,0 +1,11 @@
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.time.LocalTime;
+
+public interface IServerClock extends Remote {
+
+	LocalTime getClock() throws RemoteException;
+
+	void setClock(LocalTime clockClientTime, long nano) throws RemoteException;
+}
